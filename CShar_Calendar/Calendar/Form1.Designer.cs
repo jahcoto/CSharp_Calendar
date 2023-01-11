cd1b9ad6014cf5371558c7ee26456dd49d2aaa6c
jahcoto <jahcoto@gmail.com>
2023-01-11 01:48:52 -0600

    Update

--- CShar_Calendar/Calendar/Form1.Designer.cs
+++ CShar_Calendar/Calendar/Form1.Designer.cs
@@ -29,18 +29,38 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtYear = new System.Windows.Forms.TextBox();
-            this.txtMonth = new System.Windows.Forms.TextBox();
-            this.txtDay = new System.Windows.Forms.TextBox();
             this.btnGetInfo = new System.Windows.Forms.Button();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnShowResult = new System.Windows.Forms.Button();
+            this.txtBirthMonth = new System.Windows.Forms.TextBox();
+            this.txtBirthYear = new System.Windows.Forms.TextBox();
+            this.txtBirthDate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBirthDay = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblBirthMonth = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.lblBirthMonth);
+            this.groupBox1.Controls.Add(this.txtBirthDay);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.btnShowResult);
+            this.groupBox1.Controls.Add(this.txtBirthMonth);
+            this.groupBox1.Controls.Add(this.txtBirthYear);
+            this.groupBox1.Controls.Add(this.txtBirthDate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.btnGetInfo);
             this.groupBox1.Controls.Add(this.txtDay);
             this.groupBox1.Controls.Add(this.txtMonth);
@@ -48,77 +68,179 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(350, 10);
+            this.groupBox1.Location = new System.Drawing.Point(467, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(400, 400);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(533, 492);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Year:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 65);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Month:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 100);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Day:";
-            // 
-            // txtYear
-            // 
-            this.txtYear.Location = new System.Drawing.Point(100, 30);
-            this.txtYear.Name = "txtYear";
-            this.txtYear.Size = new System.Drawing.Size(150, 20);
-            this.txtYear.TabIndex = 3;
-            // 
-            // txtMonth
-            // 
-            this.txtMonth.Location = new System.Drawing.Point(100, 65);
-            this.txtMonth.Name = "txtMonth";
-            this.txtMonth.Size = new System.Drawing.Size(150, 20);
-            this.txtMonth.TabIndex = 4;
-            // 
-            // txtDay
-            // 
-            this.txtDay.Location = new System.Drawing.Point(100, 100);
-            this.txtDay.Name = "txtDay";
-            this.txtDay.Size = new System.Drawing.Size(150, 20);
-            this.txtDay.TabIndex = 5;
-            // 
             // btnGetInfo
             // 
-            this.btnGetInfo.Location = new System.Drawing.Point(100, 137);
+            this.btnGetInfo.Location = new System.Drawing.Point(133, 169);
+            this.btnGetInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGetInfo.Name = "btnGetInfo";
-            this.btnGetInfo.Size = new System.Drawing.Size(150, 23);
+            this.btnGetInfo.Size = new System.Drawing.Size(200, 28);
             this.btnGetInfo.TabIndex = 6;
             this.btnGetInfo.Text = "Get Info";
             this.btnGetInfo.UseVisualStyleBackColor = true;
             this.btnGetInfo.Click += new System.EventHandler(this.btnGetInfo_Click);
             // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(133, 123);
+            this.txtDay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(199, 22);
+            this.txtDay.TabIndex = 5;
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(133, 80);
+            this.txtMonth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(199, 22);
+            this.txtMonth.TabIndex = 4;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(133, 37);
+            this.txtYear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(199, 22);
+            this.txtYear.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Day:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Month:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Year:";
+            // 
+            // btnShowResult
+            // 
+            this.btnShowResult.Location = new System.Drawing.Point(133, 400);
+            this.btnShowResult.Margin = new System.Windows.Forms.Padding(4);
+            this.btnShowResult.Name = "btnShowResult";
+            this.btnShowResult.Size = new System.Drawing.Size(200, 28);
+            this.btnShowResult.TabIndex = 13;
+            this.btnShowResult.Text = "Show Result";
+            this.btnShowResult.UseVisualStyleBackColor = true;
+            this.btnShowResult.Click += new System.EventHandler(this.btnShowResult_Click);
+            // 
+            // txtBirthMonth
+            // 
+            this.txtBirthMonth.Location = new System.Drawing.Point(133, 300);
+            this.txtBirthMonth.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBirthMonth.Name = "txtBirthMonth";
+            this.txtBirthMonth.Size = new System.Drawing.Size(100, 22);
+            this.txtBirthMonth.TabIndex = 12;
+            // 
+            // txtBirthYear
+            // 
+            this.txtBirthYear.Location = new System.Drawing.Point(133, 257);
+            this.txtBirthYear.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBirthYear.Name = "txtBirthYear";
+            this.txtBirthYear.Size = new System.Drawing.Size(199, 22);
+            this.txtBirthYear.TabIndex = 11;
+            // 
+            // txtBirthDate
+            // 
+            this.txtBirthDate.Location = new System.Drawing.Point(133, 214);
+            this.txtBirthDate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(199, 22);
+            this.txtBirthDate.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 300);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Birth Month:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 257);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Birth Year:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 214);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Birth Date:";
+            // 
+            // txtBirthDay
+            // 
+            this.txtBirthDay.Location = new System.Drawing.Point(133, 335);
+            this.txtBirthDay.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBirthDay.Name = "txtBirthDay";
+            this.txtBirthDay.Size = new System.Drawing.Size(199, 22);
+            this.txtBirthDay.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 335);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Birth Day:";
+            // 
+            // lblBirthMonth
+            // 
+            this.lblBirthMonth.AutoSize = true;
+            this.lblBirthMonth.Location = new System.Drawing.Point(258, 300);
+            this.lblBirthMonth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBirthMonth.Name = "lblBirthMonth";
+            this.lblBirthMonth.Size = new System.Drawing.Size(0, 16);
+            this.lblBirthMonth.TabIndex = 16;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
@@ -137,6 +259,16 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBirthMonth;
+        private System.Windows.Forms.TextBox txtBirthDay;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnShowResult;
+        private System.Windows.Forms.TextBox txtBirthMonth;
+        private System.Windows.Forms.TextBox txtBirthYear;
+        private System.Windows.Forms.TextBox txtBirthDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- CShar_Calendar/Calendar/Form1.cs
+++ CShar_Calendar/Calendar/Form1.cs
@@ -25,5 +25,63 @@ namespace Calendar
             txtDay.Text = gCal.GetDayOfMonth(DateTime.Now).ToString();
 
         }
+
+        private void btnShowResult_Click(object sender, EventArgs e)
+        {
+            txtBirthDay.Text = txtBirthDate.Text.Substring(0,2);
+            txtBirthMonth.Text = txtBirthDate.Text.Substring(3,2);
+            int i = txtBirthDate.Text.Length;
+            txtBirthYear.Text = txtBirthDate.Text.Substring(i-4, 4);
+            int month;  int.TryParse(txtBirthMonth.Text, out month);
+            
+            
+            lblBirthMonth.Text = monthNumName(month);
+        }
+    }
+
+    public string monthNumName(int monthNumber) 
+    {
+        string monthText = "";
+        switch (monthNumber)
+        {
+            case 1:
+                monthText = "January";
+                break;
+            case 2:
+                monthText = "February";
+                break;
+            case 3:
+                monthText = "March";
+                break;
+            case 4:
+                monthText = "Abril";
+                break;
+            case 5:
+                monthText = "May";
+                break;
+            case 6:
+                monthText = "June";
+                break;
+            case 7:
+                monthText = "July";
+                break;
+            case 8:
+                monthText = "August";
+                break;
+            case 9:
+                monthText = "September";
+                break;
+            case 10:
+                monthText = "October";
+                break;
+            case 11:
+                monthText = "November";
+                break;
+            case 12:
+                monthText = "December";
+                break;
+        }
+
+        return monthText;
     }
 }
